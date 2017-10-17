0bbca46119651bd08ee0d23162496299ea2e9836
LB <feixue0213@live.com>
2017-10-17 11:30:24 +0800

    修改加入我们

--- suremail/src/components/joinus.vue
+++ suremail/src/components/joinus.vue
@@ -4,9 +4,31 @@
             <img v-bind:src="joinbanner" />
         </div>
         <div class="main">
+          <div class="join-title">
+            <img :src="title" />
+          </div>
+          <div class="join-content">
             <div class="side">
-                <img :src="more" />
+              <img :src="more" />
             </div>
+            <div class="employ-info">
+              <div class="info-item" v-for="item in items">
+                <img :src="item.pic" />
+              </div>
+            </div>
+          </div>
+          <div class="public">
+            <div class="public-title">
+              <img :src="public" />
+            </div>
+            <div class="code">
+              <img :src="code" />
+            </div>
+            <div class="code-desc">
+              <img :src="codedesc" />
+            </div>
+          </div>
+          
         </div>
     </div>
 </template>
@@ -17,7 +39,28 @@
       data () {
         return {
           joinbanner: '/static/img/joinus/banner.png',
-          more: '/static/img/joinus/more.png'
+          more: '/static/img/joinus/more.png',
+          title: '/static/img/joinus/title.png',
+          public: '/static/img/joinus/public.png',
+          code: '/static/img/joinus/code.png',
+          codedesc: '/static/img/joinus/code-desc.png',
+          items: [
+            {
+              name: 'UI',
+              pic: '/static/img/joinus/1.png',
+              alt: 'UI'
+            },
+            {
+              name: 'UI',
+              pic: '/static/img/joinus/2.png',
+              alt: 'UI'
+            },
+            {
+              name: 'UI',
+              pic: '/static/img/joinus/3.png',
+              alt: 'UI'
+            }
+          ]
         }
       }
     }
@@ -25,15 +68,72 @@
 
 <style scoped>
 .join-banner>img{
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 .main{
-    height: auto;
-    width: 100%;
+  height:3800px;
+  width:1200px;
+  padding-top:100px;
+  margin: 0 auto;
+}
+.join-title>img{
+  display: block;
+  margin:0 auto;
 }
 .side>img{
-   width: 100%;
-   height: 100%; 
+   width:56px;
+   margin-right:10px;
+}
+.employ-info{
+  width:1100px;
+  margin-top:80px;
+}
+.side,.employ-info{
+  float: left;
+}
+.info-item:nth-child(1){
+  margin-top:50px;
+}
+.info-item{
+  margin-bottom:120px;
+  animation:pulse 2s ease-in-out infinite;
+}
+@keyframes pulse
+{
+  0% {box-shadow: 0 0 25px rgba(0,0,0,0.12),0 0 45px rgba(0,0,0,0.24);}
+  50% {box-shadow: 0 0 55px rgba(0,0,0,0.12),0 0 75px rgba(0,0,0,0.24);}
+  100% {box-shadow: 0 0 25px rgba(0,0,0,0.12),0 0 45px rgba(0,0,0,0.24);}
+}
+@-webkit-keyframes pulse
+{
+  0% {box-shadow: 0 0 25px rgba(0,0,0,0.12),0 0 45px rgba(0,0,0,0.24);}
+  50% {box-shadow: 0 0 55px rgba(0,0,0,0.12),0 0 75px rgba(0,0,0,0.24);}
+  100% {box-shadow: 0 0 25px rgba(0,0,0,0.12),0 0 45px rgba(0,0,0,0.24);}
+}
+.info-item>img{
+  width: 100%;
+}
+.public{
+  margin-top:100px;
+}
+.public-title>img{
+  display: block;
+  margin: 0 auto;
+}
+.code{
+  margin-top:130px;
+}
+.code>img{
+  display: block;
+  margin:0 auto;
+  border:solid 20px lightgrey;
+}
+.code-desc{
+  margin-top:80px;
+}
+.code-desc>img{
+  display: block;
+  margin:0 auto;
 }
 </style>
